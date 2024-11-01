f5c8bf0625242b64f6ad199983942fe512aea09c
leogeney <ljgeneyl@ufpso.edu.co>
2024-11-01 17:26:59 -0500

    texto personalizado y agregar iconos y clases de tailwind

--- enrutamiento/astro.config.mjs
+++ enrutamiento/astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
    output: 'server',
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
-
